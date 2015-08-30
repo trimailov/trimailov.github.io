3cc2b6249fef4fd28a9545a8781469a6f06ff289
Justas T <j.trimailovas@gmail.com>
2015-08-30 22:25:48 +0300

    Create basic custom theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,4 +32,6 @@ AUTHOR_FEED_RSS = None
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+
+THEME = 'theme'
